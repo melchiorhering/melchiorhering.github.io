c6224e52688e0811b59244e3666f70f914aeb186
Stijn Hering <stijn@hering.nl>
2024-12-16 16:32:53 +0100

    fix: updating

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -42,18 +42,18 @@ export const about =
 	'I’m a versatile Data Scientist with a strong background in Machine Learning, AI, Data Engineering, Web Development, and Cloud Computing. Technology excites me, and I have a knack for quickly picking up and applying new innovations. I balance creativity with practicality, aiming to deliver the best possible solutions. I’m proficient in a range of programming languages and cloud tools, and I enjoy taking on interdisciplinary projects where I can contribute in multiple roles. I’m committed to continuous learning, making sure I stay ahead of the curve in this fast-evolving field.'
 
 // Static Blog Posts (as a fallback or default)
-export const staticBlogPosts: BlogPost[] = [
-	// {
-	// 	title: 'The Rust programming language',
-	// 	url: 'https://medium.com/auraidata/the-rust-programming-language-ff4c4627364',
-	// 	imagePath: 'https://miro.medium.com/v2/resize:fit:720/format:webp/0*mSK2gjXEpD4tdFxe'
-	// }
-	// Add more articles if needed
-]
+// export const staticBlogPosts: BlogPost[] = [
+// 	// {
+// 	// 	title: 'The Rust programming language',
+// 	// 	url: 'https://medium.com/auraidata/the-rust-programming-language-ff4c4627364',
+// 	// 	imagePath: 'https://miro.medium.com/v2/resize:fit:720/format:webp/0*mSK2gjXEpD4tdFxe'
+// 	// }
+// 	// Add more articles if needed
+// ]
 
 // Fetch dynamic Medium posts and merge with static posts
 export const allBlogPosts: Promise<BlogPost[]> = (async () => {
 	const username = 'stijn.hering' // Medium username
 	const mediumPosts = await fetchMediumPosts(username)
-	return [...mediumPosts, ...staticBlogPosts] // Merge with static posts
+	return [...mediumPosts] // Merge with static posts
 })()
