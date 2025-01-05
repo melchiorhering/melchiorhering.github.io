ee033ccea8cd681727caaa49449417024a95ef97
Stijn Hering <stijn@hering.nl>
2025-01-05 13:34:50 +0100

    update: added bluesky

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,38 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-	extends: ['plugin:astro/recommended'],
+	extends: [
+		'plugin:astro/recommended',
+		'plugin:@typescript-eslint/recommended' // Ensure TypeScript recommendations are included
+	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		tsconfigRootDir: __dirname,
 		sourceType: 'module',
-		ecmaVersion: 'latest'
+		ecmaVersion: 'latest',
+		extraFileExtensions: ['.astro']
 	},
+	plugins: ['@typescript-eslint'], // Ensure TypeScript plugin is loaded
 	overrides: [
 		{
 			files: ['*.astro'],
 			parser: 'astro-eslint-parser',
 			parserOptions: {
 				parser: '@typescript-eslint/parser',
-				extraFileExtensions: ['.astro']
+				extraFileExtensions: ['.astro'],
+				sourceType: 'module',
+				ecmaVersion: 'latest'
 			},
+			extends: ['plugin:astro/recommended', 'plugin:@typescript-eslint/recommended'],
 			rules: {
-				// override/add rules settings here, such as:
-				// "astro/no-set-html-directive": "error"
+				// Add Astro-specific or TypeScript-specific rules here
+				// Example:
+				// 'astro/no-set-html-directive': 'error'
 			}
 		}
-	]
+	],
+	rules: {
+		// Global rules for your project
+		'no-unused-vars': 'off', // Let TypeScript handle unused vars
+		'@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+	}
 }
